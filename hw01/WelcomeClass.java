6904c08fe2a344a84cca3a08ae6e32aa45ee8cf6
E-Wilkinson <emw316@lehigh.edu>
2015-01-23 22:38:14 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Emily Wilkinson
+//HW 1
+//Welcome Class Java Program
+//
+//  first compile program
+//      javac WelcomeClass.java
+//  run program
+//      java WelcomeClass//
+
+//  define class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the welcome statement with design
+    System.out.println("    -----------     ");
+    System.out.println("    | WELCOME |     ");
+    System.out.println("    -----------     ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-E--M--W--3--1--6->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This summer I travelled to Moldova for an Iacocca Internship.");
+    System.out.println("There I visited a winery with the longest cellar complex in the world.");
+    
+    }
+    
+}
