59425668bf4795701c5d732798c6f44b69d90a96
E-Wilkinson <emw316@lehigh.edu>
2015-02-17 23:49:15 +0000

    updated Banking.java

--- hw04/Banking.java
+++ hw04/Banking.java
@@ -8,13 +8,13 @@
 //  java Banking
 //
 //  import scanner
-    import java.util.scanner;
+    import java.util.Scanner;
     
 //  define class
 public class Banking{
         
 //  add main method
-    public static main (String[] args){
+    public static void main (String[] args){
 
 //  declare Scanner
     Scanner myScanner;
@@ -22,18 +22,52 @@ public class Banking{
     myScanner = new Scanner (System.in );
 //  generate random amount in banking acct
     int randomAmt = (int)(5000*Math.random()+1000);
+    int balance =randomAmt;
+    int choice = 10;
+    String userInput;
 //  prompt regarding money
+    System.out.println("Your balance is $"+balance+".");
     System.out.println("Do you want to deposit money, withdraw money, or view your balance?");
-    System.out.println("Please type: 'deposit' or 'withdraw' or 'balance' ");
-    boolean deposit, withdraw, balance;
-    switch (deposit){
-        case 1:
+    System.out.println("Please type: 'deposit' or 'withdraw' or 'view balance' ");
+    userInput = myScanner.next();
+    if (userInput.equalsIgnoreCase("deposit")){
+        choice = 0;
+    }
+    else if (userInput.equalsIgnoreCase("withdraw")){
+        choice = 1;
+    }
+    else if (userInput.equalsIgnoreCase("view balance")){
+        choice = 2;
+    }
+    switch (choice){
+        case 0:
             System.out.println(" How much do you wish to deposit? ");
-            int valDeposit = myScanner.nextDouble();
+            int userDeposit = myScanner.nextInt();
+            if(userDeposit <= 0){
+                System.out.println(" Please enter an amount greater than 0. ");
+                userDeposit = myScanner.nextInt();
+            }
+            if(userDeposit > 0){
+                balance = randomAmt+userDeposit;
+                System.out.println(" Your new balance is $"+balance+".");
+            }
+            break;
+        case 1:
+            System.out.println(" How much do you wish to withdraw? ");
+            int userWithdraw = myScanner.nextInt();
+            if (userWithdraw > balance){
+                System.out.println("Please do not withdraw greater than your balance $"+balance+".");
+                userWithdraw = myScanner.nextInt();
+            }
+            if (userWithdraw <= balance);{
+                balance = randomAmt - userWithdraw;
+                System.out.println("Your remaining balance is $"+balance+".");
+            }
             break;
         case 2:
-            System.out.println(" Please enter a number > 0 ");
-            break;
+            System.out.println("Your balance is $"+balance+".");
+        default: 
+            System.out.println(" Invalid number: Please enter a valid answer ");
     }
     
     }
