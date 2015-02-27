09ad1d9d8eec61eef305aabdd0242e4546d6d8aa
E-Wilkinson <emw316@lehigh.edu>
2015-02-27 16:02:04 +0000

    updated smilegenerator

---
+++ lab06-SmileGenerator/SmileGenerator.java
@@ -0,0 +1,48 @@
+//Emily Wilkinson
+//
+// Smile Generator Program
+//
+// compile and run program
+//
+//define class
+public class SmileGenerator {
+
+//	add main method
+	public static void main (String[] args) {
+/*
+//	while loop print 5 smiley faces
+	int counter = 0;
+	while (counter < 6) {
+		System.out.print(":)");
+    	counter++;
+	}//end while loop
+	System.out.println("");
+
+//	random number of smiley faces
+
+	int randomNumber;
+	
+	while (counter > 0 && counter <100){
+		randomNumber = (int)(100*Math.random()+1);
+		int a=0;
+		while ( a<31){
+			System.out.print(":)");
+			a++;
+			counter++;
+		}//end while loop
+		System.out.println("");
+	}//end while loop
+	*/
+	int counter = 0;
+	while ( counter <11){
+		System.out.print(":)");
+		int b=0;
+		while (b<3){
+			System.out.print(":)");
+			b++;
+			counter++;
+		}//end while loop
+		System.out.println("");
+	}//end while loop
+	}//end main method
+}//end class
